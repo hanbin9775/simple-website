015bb3ea40696c399c8f326760ebd1085f496ffa
hanbin9775 <dnffkrktyd@gmail.com>
2021-02-19 01:33:20 +0900

    first commit, eslint, prettier, typescript setting applied

---
+++ src/views/home/Home.styled.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  padding: 10px;
+`;
+
+export const HomeHeader = styled.h1``;
---
+++ src/views/home/Home.tsx
@@ -0,0 +1,13 @@
+/**
+ * @description home 화면
+ */
+import React from "react";
+import * as s from "./Home.styled";
+
+const Home = (): JSX.Element => (
+  <s.HomeContainer>
+    <s.HomeHeader>부슬&빈의 페이지 </s.HomeHeader>
+  </s.HomeContainer>
+);
+
+export default Home;
