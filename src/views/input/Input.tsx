c9f5a119935661e1ac90567dd116095b82d603e4
hanbin9775 <dnffkrktyd@gmail.com>
2021-03-03 01:35:35 +0900

    [#3] react-icons 모듈 설치 후 Input View input label, box, next btn 추가

--- src/views/input/Input.tsx
+++ src/views/input/Input.tsx
@@ -2,6 +2,8 @@
  * @description home 화면
  */
 import React from "react";
+import { BsPerson } from "react-icons/bs";
+import { BiTime } from "react-icons/bi";
 import * as s from "./Input.styled";
 
 const Input = (): JSX.Element => {
@@ -9,13 +11,24 @@ const Input = (): JSX.Element => {
     <s.InputContainer>
       <s.DescribeText>당신의 이름과 나이를 입력하세요</s.DescribeText>
       <s.InputWrapper>
+        <BsPerson size={48} />
         <s.InputLabel>나이</s.InputLabel>
         <s.InputBox />
       </s.InputWrapper>
       <s.InputWrapper>
-        <s.InputLabel>나이</s.InputLabel>
+        <BiTime size={48} />
+        <s.InputLabel>이름</s.InputLabel>
         <s.InputBox />
       </s.InputWrapper>
+      <s.ButtonWrapper>
+        <s.NextButton
+          to={{
+            pathname: "/",
+          }}
+        >
+          Next
+        </s.NextButton>
+      </s.ButtonWrapper>
     </s.InputContainer>
   );
 };
