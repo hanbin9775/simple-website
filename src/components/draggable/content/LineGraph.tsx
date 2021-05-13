b2a756eb06d5ec35525a322a80eb61bc8d8085bb
hanbin9775 <dnffkrktyd@gmail.com>
2021-05-13 23:32:11 +0900

    [#11] LineGraph 영역 path 및 gradient fill 구현. 위치 정보는 하드코딩

--- src/components/draggable/content/LineGraph.tsx
+++ src/components/draggable/content/LineGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "@material-ui/core/Slider";
 import * as s from "./Content.styled";
 
@@ -12,6 +12,8 @@ const LineGraph = (): JSX.Element => {
     50,
     50,
   ]);
+  const [pathInfo, setPathInfo] = useState<string>("");
+  const [fillInfo, setFillInfo] = useState<string>("");
 
   const handleChange = (
     event: React.ChangeEvent<Record<string, unknown>>,
@@ -25,39 +27,40 @@ const LineGraph = (): JSX.Element => {
     setThumbValues(tempValues);
   };
 
+  useEffect(() => {
+    let newPathInfo = thumbValues
+      .map((value, index) => {
+        if (index === 0) {
+          return `M${21} ${200 - value * 2}`;
+        }
+        return `L${21 + index * 48.9} ${200 - value * 2}`;
+      })
+      .join("");
+    setPathInfo(newPathInfo);
+    newPathInfo += "L314.4 200 L21 200";
+    setFillInfo(newPathInfo);
+  }, [thumbValues]);
+
   return (
     <s.SliderWrapper>
       <s.SliderGraphCanvas
         width={window.innerWidth > 468 ? 428 : window.innerWidth - 40}
         height="200"
       >
-        <g className="argos">
-          <title>ARGOS_logo</title>
-          <g className="ldl-scale" id="svg_1" transform="rotate(-180 50 45)">
-            <path
-              d="m50,58.2c-20,0 -36.6,-15.4 -41.2,-20.1l-0.1,-0.1c-0.4,-0.4 -1,-1 -1.4,-1.9c-0.6,-1.3 0,-2.7 1.2,-3.3c1.3,-0.6 2.7,0 3.3,1.2c0,0.1 0.3,0.3 0.4,0.4l0.2,0.2c4.2,4.3 19.6,18.6 37.6,18.6s33.4,-14.2 37.6,-18.6l0.2,-0.2l0.5,-0.5c0.6,-1.3 2,-1.8 3.3,-1.3c1.3,0.6 1.8,2.1 1.3,3.3c-0.4,0.9 -1,1.5 -1.5,1.9l-0.1,0.1c-4.7,4.9 -21.3,20.3 -41.3,20.3z"
-              fill="#0BB3E5"
-              id="eye_line"
+        <defs>
+          <linearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop
+              offset="0%"
+              style={{ stopColor: "rgba(85, 0, 255, 0.44)", stopOpacity: 1 }}
             />
-          </g>
-          <path
-            d="M10,45 A53,55 0 0,1 90,45"
-            stroke="transparent"
-            fill="none"
-            id="eye_text_line"
-          />
-          <text x="3" letterSpacing="13" fill="#0BB3E5" />
-          <ellipse
-            cx="50"
-            cy="50"
-            fill="#fff"
-            stroke="#0BB3E5"
-            strokeWidth="8"
-            id="eye_ball"
-            rx="8"
-            ry="8"
-          />
-        </g>
+            <stop
+              offset="100%"
+              style={{ stopColor: "rgb(255,255,255)", stopOpacity: 1 }}
+            />
+          </linearGradient>
+        </defs>
+        <path d={pathInfo} stroke="#5500FF" strokeWidth="3" fill="none" />
+        <path d={fillInfo} stroke="none" fill="url(#grad1)" />
       </s.SliderGraphCanvas>
       {thumbValues.map((value, index) => (
         <Slider
