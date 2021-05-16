f5e15cc98d3ad10fffb89408ff4a102987a8a596
hanbin9775 <dnffkrktyd@gmail.com>
2021-05-16 23:39:59 +0900

    [#14] Video Poster applied

--- src/components/bg_video/BackGroundVideo.tsx
+++ src/components/bg_video/BackGroundVideo.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BackGroundVideoProp } from "type";
+import PosterImage from "assets/image/title_kor.png";
 import VideoSection from "./BackGroundVideo.styled";
 
 const BackGroundVideo = ({ width, src }: BackGroundVideoProp): JSX.Element => {
   return (
     <VideoSection>
-      <video loop muted autoPlay playsInline width={width}>
+      <video poster={PosterImage} loop muted autoPlay playsInline width={width}>
         <source src={src} type="video/mp4" />
         <track
           src="captions_en.vtt"
